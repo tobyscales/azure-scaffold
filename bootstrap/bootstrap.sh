4ea77f112baed136a2469c5b6fd4a10ebca513b2
tescales <toby.scales@microsoft.com>
2019-09-26 15:24:34 -0700

    add retry to bootstrap, deploy.azcli v1

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -6,7 +6,30 @@ echo Location: $AZURE_LOCATION
 echo Resource Group: $AZURE_RESOURCE_GROUP
 echo Configurations from: $CONFIG_URL
 
-az login --identity
+# cribbed from http://fahdshariff.blogspot.com/2014/02/retrying-commands-in-shell-scripts.html
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -r cmd="$@"
+    local -i attempt_num=1
+    until $cmd
+    do
+        if ((attempt_num==max_attempts))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $((attempt_num++))
+        fi
+    done
+}
+
+retry 5 az login --identity
+
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
